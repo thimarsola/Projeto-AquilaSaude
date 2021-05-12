8275707eba608ba8e0b37f2647bbae562853285e
Thiago <tmarsol94@gmail.com>
2021-05-12 16:50:49 -0300

    CONCLUSION OF STYLE TEAM SECTION

--- template-parts/home/home-team.php
+++ template-parts/home/home-team.php
@@ -5,10 +5,8 @@
             <!-- image -->
             <div class="team__row__image">
                 <picture>
-                    <source srcset="<?= get_template_directory_uri(); ?>/assets/images/profissionais-aquila.webp"
-                        type="image/webp">
-                    <img src="<?= get_template_directory_uri(); ?>/assets/images/profissionais-aquila.png"
-                        alt="Profissionais Áquila Saúde" loading='lazy'>
+                    <source srcset="<?= get_template_directory_uri(); ?>/assets/images/profissionais-aquila.webp" type="image/webp">
+                    <img src="<?= get_template_directory_uri(); ?>/assets/images/profissionais-aquila.png" alt="Profissionais Áquila Saúde" loading='lazy'>
                 </picture>
             </div>
             <!-- end of image -->
@@ -21,13 +19,11 @@
                 </header>
                 <!-- end of header -->
 
-                <p>A <b>clínica Áquila Saúde</b> possui uma estrutura diferenciada, tecnologia de última geração e reúne
-                    profissionais atualizados e altamente capacitados de todas as áreas da Odontologia.</p>
+                <p>A <b>clínica Áquila Saúde</b> possui uma estrutura diferenciada, tecnologia de última geração e reúne profissionais atualizados e altamente capacitados de todas as áreas da Odontologia.</p>
 
                 <!-- link -->
                 <div class="team__row__body__link">
-                    <a href="<?= get_permalink(get_page_by_title('Profissionais')); ?>" class="btn btn-blue-500"
-                        target="_blank">Conheça a nossa Equipe</a>
+                    <a href="<?= get_permalink(get_page_by_title('Profissionais')); ?>" class="btn btn-blue-500" target="_blank">Conheça a nossa Equipe</a>
                 </div>
                 <!-- end of link -->
             </div>
