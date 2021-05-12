f3a046557306d045fbc7d6a04279e8ef2fd4c863
Thiago <tmarsol94@gmail.com>
2021-05-12 16:33:29 -0300

    CONCLUSION OF STYLE INVISALIGN SECTION

--- template-parts/home/home-invisalign.php
+++ template-parts/home/home-invisalign.php
@@ -1,4 +1,4 @@
-<section class="invisalign">
+<section class="invisalign bg-dark-900">
     <div class="container">
         <!-- header -->
         <header class="d-none">
@@ -13,10 +13,8 @@
                 <!-- image -->
                 <div class="invisalign__row__card__image">
                     <picture>
-                        <source srcset="<?= get_template_directory_uri(); ?>/assets/images/bruno-gagliasso.webp"
-                            type="image/webp">
-                        <img src="<?= get_template_directory_uri(); ?>/assets/images/bruno-gagliasso.png"
-                            alt="Bruno Gagliasso - Insivalign" loading="lazy">
+                        <source srcset="<?= get_template_directory_uri(); ?>/assets/images/bruno-gagliasso.webp" type="image/webp">
+                        <img src="<?= get_template_directory_uri(); ?>/assets/images/bruno-gagliasso.png" alt="Bruno Gagliasso - Insivalign" loading="lazy">
                     </picture>
                 </div>
                 <!-- end of image -->
@@ -29,12 +27,9 @@
                     </header>
                     <!-- end of header -->
 
-                    <p>O Invisalign é um sistema que utiliza alinhadores transparentes para corrigir as posições
-                        dentárias.</p>
+                    <p>O Invisalign é um sistema que utiliza alinhadores transparentes para corrigir as posições dentárias.</p>
                     <br>
-                    <p>Usando o scanner iTero Element<sup>&REG;</sup>, seu ortodontista poderá digitalizar o seu sorriso
-                        em 3D de forma rápida e precisa e poderá fazer um mapeamento do seu tratamento personalizado
-                        especialmente para você.</p>
+                    <p>Usando o scanner iTero Element<sup>&REG;</sup>, seu ortodontista poderá digitalizar o seu sorriso em 3D de forma rápida e precisa e poderá fazer um mapeamento do seu tratamento personalizado especialmente para você.</p>
                 </div>
                 <!-- end of body -->
             </article>
@@ -54,9 +49,7 @@
                     </header>
                     <!-- end of header -->
 
-                    <p>O valor do tratamento ortodôntico com Invisalign é determinado pelas necessidades do seu caso,
-                        como o tipo de correção que será feita, a complexidade e a duração do tratamento. Somente o
-                        ortodontista, após uma avaliação, poderá informar o valor do tratamento com Invisalign.</p>
+                    <p>O valor do tratamento ortodôntico com Invisalign é determinado pelas necessidades do seu caso, como o tipo de correção que será feita, a complexidade e a duração do tratamento. Somente o ortodontista, após uma avaliação, poderá informar o valor do tratamento com Invisalign.</p>
 
                     <!-- link -->
                     <div class="invisalign__row__card__body__link">
@@ -71,10 +64,8 @@
                 <!-- image -->
                 <div class="invisalign__row__card__image">
                     <picture>
-                        <source srcset="<?= get_template_directory_uri(); ?>/assets/images/aparelho-invisalign.webp"
-                            type="image/webp">
-                        <img src="<?= get_template_directory_uri(); ?>/assets/images/aparelho-invisalign.jpg"
-                            alt="Bruno Gagliasso - Invisalign" loading="lazy">
+                        <source srcset="<?= get_template_directory_uri(); ?>/assets/images/aparelho-invisalign.webp" type="image/webp">
+                        <img src="<?= get_template_directory_uri(); ?>/assets/images/aparelho-invisalign.jpg" alt="Bruno Gagliasso - Invisalign" loading="lazy">
                     </picture>
                 </div>
                 <!-- end of image -->
