7a1b60cdf4fce02eb0078980e5434d4aa376cf6b
Thiago <tmarsol94@gmail.com>
2021-05-17 16:19:20 -0300

    CONCLUSION OF ADJUST IN STYLE ABOUT SECTION

--- template-parts/home/home-about.php
+++ template-parts/home/home-about.php
@@ -11,15 +11,9 @@
                 </header>
                 <!-- end of  header -->
 
-                <p>A <b>Clínica Áquila Saúde</b> foi inaugurada em 2020, e conta com ambientes cuidadosamente
-                    personalizados,
-                    aconchegantes e acolhedores, oferecendo ao paciente uma experiência agradável durante todas as fases
-                    do seu tratamento odontológico.</p>
+                <p>A <b>Clínica Áquila Saúde</b> foi inaugurada em 2020, e conta com ambientes cuidadosamente personalizados,  aconchegantes e acolhedores, oferecendo ao paciente uma experiência agradável durante todas as fases do seu tratamento odontológico.</p>
                 <br>
-                <p>O fundador Dr. Marcos Vinícius Brandão, possui mais de 35 anos de odontologia atuando em São Caetano
-                    do Sul, e realizou um sonho de transformar um consultório odontológico em uma clinica de referencia
-                    em odontologia, com profissionais renomados. Pensando na saúde e bem estar dos seus clientes
-                    instalou uma sala de estética.</p>
+                <p>O fundador Dr. Marcos Vinícius Brandão, possui mais de 35 anos de odontologia atuando em São Caetano do Sul, e realizou um sonho de transformar um consultório odontológico em uma clinica de referencia em odontologia, com profissionais renomados. Pensando na saúde e bem estar dos seus clientes instalou uma sala de estética.</p>
                 <br>
                 <p>A <b>Áquila Saúde</b> é referencia em odontologia e saúde em São Caetano do Sul e região.</p>
             </div>
@@ -33,26 +27,25 @@
 
                     <!-- video -->
                     <div class="about__row__information__grid__video">
-                        <iframe src="https://www.youtube.com/embed/LIGar-f6TSk" title="Conheça a Clínica Áquila Saúde"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                            allowfullscreen></iframe>
+                        <iframe src="https://www.youtube.com/embed/LIGar-f6TSk" title="Conheça a Clínica Áquila Saúde" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
                     <!-- video -->
 
                     <!-- ceo -->
                     <div class="about__row__information__grid__ceo">
-                        <picture>
-                            <source srcset="<?= get_template_directory_uri(); ?>/assets/images/marcos-brandao.webp"
-                                type="image/webp">
-                            <img src="<?= get_template_directory_uri(); ?>/assets/images/marcos-brandao.jpg"
-                                alt="Dr. Marcos Vinícius Brandão" loading='lazy'>
-                        </picture>
+                        <!-- image -->
+                        <div class="about__row__information__grid__ceo__image">
+                            <picture>
+                                <source srcset="<?= get_template_directory_uri(); ?>/assets/images/marcos-brandao.webp" type="image/webp">
+                                <img src="<?= get_template_directory_uri(); ?>/assets/images/marcos-brandao.jpg" alt="Dr. Marcos Vinícius Brandão" loading='lazy'>
+                            </picture>
+                        </div>
+                        <!-- end of image -->
 
                         <!-- text -->
                         <div class="about__row__information__grid__ceo__text">
                             <p><b>Dr. Marcos Vinícius Brandão</b></p>
-                            <p><small>Clínico Geral - Especialista em endodontia e prótese há 35 anos<br>CRO/SP
-                                    30071</small></p>
+                            <p><small>Clínico Geral - Especialista em endodontia e prótese há 35 anos<br>CRO/SP 30071</small></p>
                         </div>
                         <!-- end of text -->
 
@@ -62,11 +55,8 @@
                     <!-- clinic -->
                     <div class="about__row__information__grid__clinic">
                         <picture>
-                            <source
-                                srcset="<?= get_template_directory_uri(); ?>/assets/images/clinica-aquila-entrada.webp"
-                                type="image/webp">
-                            <img src="<?= get_template_directory_uri(); ?>/assets/images/clinica-aquila-entrada.jpg"
-                                alt="Dr. Marcos Vinícius Brandão" loading='lazy'>
+                            <source srcset="<?= get_template_directory_uri(); ?>/assets/images/clinica-aquila-entrada.webp" type="image/webp">
+                            <img src="<?= get_template_directory_uri(); ?>/assets/images/clinica-aquila-entrada.jpg" alt="Dr. Marcos Vinícius Brandão" loading='lazy'>
                         </picture>
                     </div>
                     <!-- end of clinic -->
